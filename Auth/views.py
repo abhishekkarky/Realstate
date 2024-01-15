a0149d600fbc40d7eed03febec85e90ce489525e
abhishekkarky <abhishekkarki40@gmail.com>
2024-01-15 19:31:36 +0545

    Changes

--- Auth/views.py
+++ Auth/views.py
@@ -1,7 +1,4 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth import authenticate, login as auth_login
-
-from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
